7415c2bfb7ef823d22a1be640d6bc302137ef85e
Akanksha Makkar <akankshamakkar@Akankshas-MacBook-Pro.local>
2023-04-24 19:37:07 +0530

    validate license id

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,5 +1,10 @@
 package com.driver;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
 
@@ -21,12 +26,28 @@ public class CurrentAccount extends BankAccount{
     }
 
     public void validateLicenseId() throws Exception {
+        if(!tradeLicenseId.equals(tradeLicenseId.toUpperCase())) {
+            throw new Exception("Valid License can not be generated");
+        }
 
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<tradeLicenseId.length();i++) {
+            if(map.containsKey(tradeLicenseId.charAt(i))) {
+                int value = map.get(tradeLicenseId.charAt(i));
+                map.put(tradeLicenseId.charAt(i), value+1 );
+            } else {
+                map.put(tradeLicenseId.charAt(i), 1 );
+            }
+        }
+        if(Collections.max(map.values()) > (tradeLicenseId.length() + 1)/2) {
+            throw new Exception("Valid License can not be generated");
+        }
+
+        Collections.shuffle(Arrays.asList(tradeLicenseId.toCharArray()));
         // A trade license Id is said to be valid if no two consecutive characters are same
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
-
     }
 
 }
